efb597a670042d94bbbf725db4dc55fced2338bc
Ricard Villalba Burrull <ricardsax@gmail.com>
2021-09-14 19:16:36 +0200

    model page working after convert the id  coming from getId to a number it eas a string

--- src/components/model.js
+++ src/components/model.js
@@ -6,19 +6,21 @@ const Model = (props) => {
     return id;
   }
   const modelId = GetId();
-  console.log(modelId);
+  console.log(typeof modelId);
   //////////////////////
   const data = props.data;
-  //   console.log(data.models);
+
   //   console.log(props.data.models);
-  const model = data.models.find((model) => model._id === modelId);
+  const model = data.models.find((model) => model._id === parseInt(modelId));
+
   console.log(model);
   return (
     <div className="modelContainer">
-      {/* <img src={model.image} alt={model.name} />
+      <img src={`/${model.image}`} alt={model.name} />
       <div className="modelInfo">
         <p>{model.name}</p>
-      </div> */}
+        <p>{model.age}</p>
+      </div>
     </div>
   );
 };
