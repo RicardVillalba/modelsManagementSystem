c523697cb55390ec21fa7da79960beca6696ddd9
Ricard Villalba Burrull <ricardsax@gmail.com>
2021-09-15 12:45:52 +0200

    modelsList basic styles done

--- src/components/modelsList.js
+++ src/components/modelsList.js
@@ -1,15 +1,23 @@
+import "./styles/modelsList.css";
+
 const ModelsList = (props) => {
   const data = props.data;
   return (
     <div className="modelsListContainer">
-      {data.models.map((model) => (
-        <div key={model._id} model={model}>
-          <a href={`/model/${model._id}`}>
-            <img src={model.image} alt={model.name} />
-          </a>
-          <h1>{model.name}</h1>
-        </div>
-      ))}
+      <div className="modelsList">
+        {data.models.map((model) => (
+          <div key={model._id} model={model} className="modelsListModelCard">
+            <a href={`/model/${model._id}`}>
+              <img
+                src={model.image}
+                alt={model.name}
+                className="modelsListModelImage"
+              />
+              <h1 className="modelsListModelCardInfo">{model.name}</h1>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
