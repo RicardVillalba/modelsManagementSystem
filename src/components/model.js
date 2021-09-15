06909e8637168606e47bc52a0564181b27255c0b
Ricard Villalba Burrull <ricardsax@gmail.com>
2021-09-15 15:19:06 +0200

    model page basic styling done

--- src/components/model.js
+++ src/components/model.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-
+import "./styles/model.css";
 const Model = (props) => {
   function GetId() {
     const { id } = useParams();
@@ -16,10 +16,48 @@ const Model = (props) => {
   console.log(model);
   return (
     <div className="modelContainer">
-      <img src={`/${model.image}`} alt={model.name} />
-      <div className="modelInfo">
-        <p>{model.name}</p>
-        <p>{model.age}</p>
+      <div className="modelPageMainContent">
+        <img
+          src={`/${model.image}`}
+          alt={model.name}
+          className="modelPageMainImage"
+        />
+        <div className="modelPageInfoContainer">
+          <div className="modelPageInfoContent">
+            <p className="modelpageModelName">{model.name}</p>
+            <p className="modelpageModelDescription">{model.description}</p>
+            <div className="modelPageInfoDetailsContainer">
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">height</p>
+                <p className="modelsPageInfoValue">{model.height}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">bust</p>
+                <p className="modelsPageInfoValue">{model.bust}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">waist</p>
+                <p className="modelsPageInfoValue">{model.waist}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">hips</p>
+                <p className="modelsPageInfoValue">{model.hips}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">hair</p>
+                <p className="modelsPageInfoValue">{model.hair}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">eyes</p>
+                <p className="modelsPageInfoValue">{model.eyes}</p>
+              </div>
+              <div className="modelPageInfoDetails">
+                <p className="modelsPageInfoName">shoes</p>
+                <p className="modelsPageInfoValue">{model.shoes}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
