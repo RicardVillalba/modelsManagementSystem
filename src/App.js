edda6380c938fb029b0b6cbf3b8ab06a765a2364
Ricard Villalba Burrull <ricardsax@gmail.com>
2021-09-16 17:56:03 +0200

    splashscreen created using settimeout

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import "./App.css";
 import NavBar from "./components/navBar";
 import ModelsList from "./components/modelsList";
 import Model from "./components/model";
 import data from "./components/data";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SplashScreen from "./components/splashscreen";
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <Route
-          render={({ location }) => (
-            <AnimatePresence initial={false} exitBeforeEnter>
-              <Switch location={location} key={location.pathname}>
-                <Route path="/women">
-                  <ModelsList data={data} />
-                </Route>
-                <Route path="/model/:id">
-                  <Model data={data} />
-                </Route>
-              </Switch>
-            </AnimatePresence>
-          )}
-        />
-      </Router>
-    </div>
+    <>
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <div className="App">
+          <Router>
+            <NavBar />
+
+            <Route
+              render={({ location }) => (
+                <AnimatePresence initial={false} exitBeforeEnter>
+                  <Switch location={location} key={location.pathname}>
+                    <Route path="/women">
+                      <ModelsList data={data} />
+                    </Route>
+                    <Route path="/model/:id">
+                      <Model data={data} />
+                    </Route>
+                  </Switch>
+                </AnimatePresence>
+              )}
+            />
+          </Router>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/splashscreen.js
@@ -0,0 +1,7 @@
+import "./styles/splashScreen.css";
+
+const SplashScreen = () => {
+  return <p className="splashScreenLogo">mma</p>;
+};
+
+export default SplashScreen;
